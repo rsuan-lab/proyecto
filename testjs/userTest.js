879c35fc46daf12c35f6f2a5265332296a39bfd9
rsuan-lab <rsuan@bancodebogota.com.co>
2022-04-12 13:04:31 -0500

    cambios

--- testjs/userTest.js
+++ testjs/userTest.js
@@ -1,6 +1,15 @@
 import supertest from "supertest";
 import app from "../src/app"
 import test from "ava";
+import { request } from "express";
 test('guardar datos',(t)=>{
-
+    const user={ id: 11, username: 'Dr Nice' , compras:'nada',password:'1234' };
+    request(app)
+    .post('/add')
+    .expect('Content-Type', /json/)
+    .expect('Content-Length', '15')
+    .expect(200)
+    .end(function(err, res) {
+        if (err) throw err;
+     });
 })
